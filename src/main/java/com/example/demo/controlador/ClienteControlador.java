eec2957b811316c27837bfcffd5ee3e517acc098
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-09-09 02:09:54 -0500

    Carga inicial del proyecto, Back y frot conectados

---
+++ src/main/java/com/example/demo/controlador/ClienteControlador.java
@@ -0,0 +1,27 @@
+package com.example.demo.controlador;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repositorio.ClienteRepositorio;
+
+import com.example.demo.modelo.Cliente;
+
+@RestController
+@RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ClienteControlador {
+
+	@Autowired
+	private ClienteRepositorio repositorio;
+
+	@GetMapping("/clientes")
+	public List<Cliente> listarTodosLosClientes() {
+		return repositorio.findAll();
+	}
+}
---
+++ src/main/java/com/example/demo/modelo/Cliente.java
@@ -0,0 +1,83 @@
+package com.example.demo.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nombre", length = 60, nullable = false)
+	private String nombre;
+
+	@Column(name = "apellido", length = 60, nullable = false)
+	private String apellido;
+
+	@Column(name = "email", length = 60, nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "fecha_registro", length = 60, nullable = false)
+	private String fecha_registro;
+
+	public Cliente() {
+
+	}
+
+	public Cliente(Long id, String nombre, String apellido, String email, String fecha_registro) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.fecha_registro = fecha_registro;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFecha_registro() {
+		return fecha_registro;
+	}
+
+	public void setFecha_registro(String fecha_registro) {
+		this.fecha_registro = fecha_registro;
+	}
+
+}
