61de21ae6b45c02cd3a6c0e6cbec1c4a5158f273
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-09-09 16:42:03 -0500

    Metodo guardar Clientes

--- src/main/java/com/example/demo/controlador/ClienteControlador.java
+++ src/main/java/com/example/demo/controlador/ClienteControlador.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +26,11 @@ public class ClienteControlador {
 	public List<Cliente> listarTodosLosClientes() {
 		return repositorio.findAll();
 	}
+	
+	//guardar empleado
+	@PostMapping("/clientes")
+	public Cliente guardarCliente(@RequestBody Cliente cliente) {
+		return repositorio.save(cliente);
+	}
+	
 }
