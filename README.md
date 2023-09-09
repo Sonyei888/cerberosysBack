3ae97a7709018b29d37b05e1581d7144353f309a
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-09-09 02:08:00 -0500

    Eliminados todos los archivos

--- README.md
+++
@@ -1,2 +0,0 @@
-# cerberosysBack
- Sistema de información encargado de administrar, controlar y agilizar la venta de productos en los almacenes de la cadena.
--- src/main/java/com/cerberosysBack/cerberosysBack/model/dao/ClienteDao.java
+++
@@ -1,8 +0,0 @@
-package com.cerberosysBack.cerberosysBack.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cerberosysBack.cerberosysBack.model.entity.Cliente;
-
-public interface ClienteDao extends CrudRepository<Cliente, Integer>{
-}
