6479bf0358f99e51e2690719996896c636fc989e
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-09-09 17:24:13 -0500

    Metodos buscar y actualizar clientes

--- src/main/java/com/example/demo/controlador/ClienteControlador.java
+++ src/main/java/com/example/demo/controlador/ClienteControlador.java
@@ -3,15 +3,17 @@ package com.example.demo.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repositorio.ClienteRepositorio;
-
+import com.example.demo.exepciones.ResourceNotFoundException;
 import com.example.demo.modelo.Cliente;
 
 @RestController
@@ -33,4 +35,26 @@ public class ClienteControlador {
 		return repositorio.save(cliente);
 	}
 	
+	//Buscar un empleado por id
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<Cliente> obtenerClientePorId(@RequestBody Long id){
+		Cliente cliente = repositorio.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No existe el cliente con el ID: " + id));
+		return ResponseEntity.ok(cliente);
+	}
+	
+	
+	@PutMapping("/clientes/{id}")
+	public ResponseEntity<Cliente> actualizarCliente(@RequestBody Long id, @RequestBody Cliente detallesCliente){
+		Cliente cliente = repositorio.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No existe el cliente con el ID: " + id));
+		
+		cliente.setNombre(detallesCliente.getNombre());
+		cliente.setApellido(detallesCliente.getApellido());
+		cliente.setEmail(detallesCliente.getEmail());
+		cliente.setFecha_registro(detallesCliente.getFecha_registro());
+		
+		Cliente clienteActualizado = repositorio.save(cliente);
+		return ResponseEntity.ok(clienteActualizado);
+	}
 }
